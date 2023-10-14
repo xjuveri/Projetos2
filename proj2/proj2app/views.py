762b0880f390482eed9723a0f6b793212ecf4cf5
Kaiquegb <kaiqueghsilva@gmail.com>
2023-10-14 15:44:50 -0300

    imports na view

--- proj2/proj2app/views.py
+++ proj2/proj2app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.db import models
 
 # Create your views here.
