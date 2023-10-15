00f280639096ad618de196092b23ace09165c7e7
Júlia Veríssimo <juliiaveriissiimo@gmail.com>
2023-10-15 20:50:11 -0300

    resolvendo erro do git

--- proj2/proj2app/views.py
+++ proj2/proj2app/views.py
@@ -3,20 +3,14 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.db import models
-from .forms import MeuFormulario
 
 # Create your views here.
 
 def home(request):
   return render(request, 'home.html')
-  
 
 def login(request):
-    if request.method == 'POST':
-        form = MeuFormulario(request.POST)
-        if form.is_valid():
-            return redirect('home.html')
-    else:
-        form = MeuFormulario()
-
-    return render(request, 'login.html', {'form': form})
+  return render(request, 'login.html')
+  
+def cadastro(request):
+  return render(request, 'cadastro.html')
