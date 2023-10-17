b2ad239dd75decb79da7097266eada212834120c
Kaiquegb <kaiqueghsilva@gmail.com>
2023-10-16 23:02:08 -0300

    whitenoise root

--- proj2/settings.py
+++ proj2/settings.py
@@ -122,6 +122,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
