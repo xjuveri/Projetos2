53e271ce752b617b830eef321c1813bfcd104385
Luiza Calife <califeluiza@gmail.com>
2023-11-29 20:57:34 -0300

    navbar fix

--- proj2/urls.py
+++ proj2/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('prestacao/',views.prestacao, name= 'prestacao'),
-    path('eventos/', views.eventos, name = 'eventos'),
     path('grafico/', views.grafico, name = 'grafico'),
     path('inventario/', include('inventario_patrimonial.urls')),
 ]
