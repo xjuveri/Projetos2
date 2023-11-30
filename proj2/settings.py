04bd88f21b0c4f3c94db0f36d91f98915b0a827d
Luketam <luccadaveigagomes@gmail.com>
2023-11-30 05:39:00 -0300

    blg

--- proj2/settings.py
+++ proj2/settings.py
@@ -135,3 +135,5 @@ WHITENOISE_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://alecrim-dourado.azurewebsites.net/']
