b8818bc8aa1d3df8bb403e130fa9831b715e7a51
Luiza Calife <califeluiza@gmail.com>
2023-11-30 01:06:38 -0300

    lucca

--- proj2/urls.py
+++ proj2/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('inventario/', include('inventario_patrimonial.urls')),
     path('turmas/', views.turmas, name = 'turmas'),
     path('comoAjudar/', views.comoAjudar, name = 'comoAjudar'),
+    path('relatorio/', include('relatorio.urls')),
     #tipos de login
     path('login_gestor', views.login_gestor, name='login_gestor'),
     path('login_educador/', views.login_educador, name='login_educador'),
